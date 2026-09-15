ba32187bb505d89e088a034dcf789e492ba9c12d
glennece2k10-collab <glennece2k10@gmail.com>
2026-09-15 14:41:39 +0800

    Update playwright.config.ts
    GUNPLA_SITE

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.GUNPLASITE,
+    baseURL: process.env.GUNPLA_SITE,
     username: process.env.TEST_USERNAME!,
     password: process.env.TEST_PASSWORD!,
     email: process.env.TEST_EMAIL!,
